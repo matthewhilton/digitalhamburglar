12443c275a6220baf040e50bd1657f5d01111822
unxpctederr <17095477+unxpctederr@users.noreply.github.com>
2021-03-30 03:28:47 +1000

    Update proxy endpoints

--- next_frontend/hamburglar/next.config.js
+++ next_frontend/hamburglar/next.config.js
@@ -2,12 +2,14 @@ module.exports = {
     async rewrites() {
       return [
         {
-          source: '/api/:path*',
-          destination: process.env.API_ENDPOINT + '/:path*' // Proxy to Backend
+            // Proxy api requests
+            source: '/api/:path*',
+            destination: process.env.API_ENDPOINT + '/:path*'
         },
         {
+            // Proxy offer image redirects
             source: '/offerImages/:path*',
-            destination: process.env.API_ENDPOINT + '/offerImages/:path*' // Proxy to Backend
+            destination: process.env.API_ENDPOINT + '/offerImages/:path*'
           }
       ]
     }

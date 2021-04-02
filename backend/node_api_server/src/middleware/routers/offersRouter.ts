bf4852419def014ef4a38b72b1ce1b1255860a60
unxpctederr <mj.hilton@outlook.com>
2021-04-02 14:22:13 +1000

    add more error handling

--- backend/node_api_server/src/middleware/routers/offersRouter.ts
+++ backend/node_api_server/src/middleware/routers/offersRouter.ts
@@ -17,17 +17,22 @@ const mcd_api = new McdApi()
 // Start an interval to update the offers
 const updateOffers = async () => {
   console.log("Updating offers...")
-  const accounts = await api.getAllAccounts()
-  const offerChecks: Promise<Offer[]>[] = accounts.map(account => mcd_api.get_offers(account))
-  const offerCheckResponses = await Promise.all(offerChecks)
-
-  // Destruct the outer array
-  const reducer = (accumulator, currentValue) => [...currentValue, ...accumulator];
-  const offersAcrossAllAccounts = offerCheckResponses.reduce(reducer)
-
-  // Drop all offers then Save the new offers in DB 
-  await api.delete_all_offers()
-  await api.save_offers(offersAcrossAllAccounts)
+  try {
+    const accounts = await api.getAllAccounts()
+    const offerChecks: Promise<Offer[]>[] = accounts.map(account => mcd_api.get_offers(account))
+    const offerCheckResponses = await Promise.all(offerChecks)
+  
+    // Destruct the outer array
+    const reducer = (accumulator, currentValue) => [...currentValue, ...accumulator];
+    const offersAcrossAllAccounts = offerCheckResponses.reduce(reducer)
+  
+    // Drop all offers then Save the new offers in DB 
+    await api.delete_all_offers()
+    await api.save_offers(offersAcrossAllAccounts)
+  } catch(err) {
+    console.log("An error ocurred while updating offers")
+    console.log(err)
+  }
 }
 
 const updateInterval = process.env.OFFER_UPDATE_INTERVAL
@@ -38,18 +43,24 @@ setTimeout(() => {
 }, 5000)
 
 router.get('/list/groups', async(req, res, next) => {
-  const offers = await api.get_offers_simple_list()
+  try {
+    const offers = await api.get_offers_simple_list()
   
-  const offersGroupedByTitle = groupBy(offers, offer => offer.title)
-
-  const offerHashGroups = Object.keys(offersGroupedByTitle).map(offerTitle => ({
-    title: offerTitle,
-    image: offersGroupedByTitle[offerTitle][0].image,
-    count: offersGroupedByTitle[offerTitle].length,
-    hash: sha256(offerTitle).toString()
-  }))
-
-  res.json(offerHashGroups)
+    const offersGroupedByTitle = groupBy(offers, offer => offer.title)
+  
+    const offerHashGroups = Object.keys(offersGroupedByTitle).map(offerTitle => ({
+      title: offerTitle,
+      image: offersGroupedByTitle[offerTitle][0].image,
+      count: offersGroupedByTitle[offerTitle].length,
+      hash: sha256(offerTitle).toString()
+    }))
+  
+    res.json(offerHashGroups)
+  }catch(err) {
+    console.log(err)
+    res.status(500).json("Unknown error ocurred")
+  }
+  
 })
 
 // List of offers in simple format
@@ -60,34 +71,40 @@ router.get('/list', async (req, res, next) => {
 })
 
 router.get('/details', async (req, res, next) => {
-    // Takes in offer hash, determines an offer to redeem from that group and redeems it 
-    const offerHash: string = req.query.offerHash
+    try {
+      // Takes in offer hash, determines an offer to redeem from that group and redeems it 
+      const offerHash: string = req.query.offerHash
 
-    if(!offerHash) {
-      res.status(400).json("Offer hash query parameter not given")
+      if(!offerHash) {
+        res.status(400).json("Offer hash query parameter not given")
+      }
+
+      const allOffers = await api.get_offers_simple_list()
+      const offersFromThisGroup = allOffers.filter(offer => sha256(offer.title).toString() === offerHash)
+
+      // Sample one randomly to help prevent two people getting the same offer
+      const selectedOffer = sample(offersFromThisGroup)
+
+      if(!selectedOffer) {
+        res.status(500).json("Could not find offer")
+        return
+      }
+
+      if(!selectedOffer.externalId) {
+        res.status(500)
+        return
+      }
+
+      const offersDetails = await api.get_offer_more_details(selectedOffer.externalId)
+      res.json(offersDetails)
+    } catch(err) {
+      console.log(err)
+      res.status(500).json("Unknown error ocurred")
     }
-
-    const allOffers = await api.get_offers_simple_list()
-    const offersFromThisGroup = allOffers.filter(offer => sha256(offer.title).toString() === offerHash)
-
-    // Sample one randomly to help prevent two people getting the same offer
-    const selectedOffer = sample(offersFromThisGroup)
-
-    if(!selectedOffer) {
-      res.status(500).json("Could not find offer")
-      return
-    }
-
-    if(!selectedOffer.externalId) {
-      res.status(500)
-      return
-    }
-
-    const offersDetails = await api.get_offer_more_details(selectedOffer.externalId)
-    res.json(offersDetails)
 })
 
 router.get('/redeem', async (req, res) => {
+  try {
     const externalId = req.query.externalId
 
     if(!externalId) {
@@ -98,6 +115,10 @@ router.get('/redeem', async (req, res) => {
     const mcdDetails = await api.get_mcd_offer_details(externalId)
     const redemptionCode = await mcd_api.get_offer_redemption_code(mcdDetails.mcd_id, mcdDetails.mcd_prop_id, mcdDetails.profile)
     res.json(redemptionCode)
+  } catch(err) {
+    console.log(err)
+    res.status(500).json("Unknown error ocurred")
+  }
 })
 
 module.exports = router;

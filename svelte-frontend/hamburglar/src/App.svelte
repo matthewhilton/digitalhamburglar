452d04f7e6f9842ed628e2ffed1149314cace48c
unxpctederr <mj.hilton@outlook.com>
2021-11-02 04:24:05 +1000

    Add basic svelte frontend

---
+++ svelte-frontend/hamburglar/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	  import { useSWR } from "sswr";
+	  const { data: posts } = useSWR('https://jsonplaceholder.typicode.com/posts')
+</script>
+
+{#if $posts}
+  {#each $posts as post (post.id)}
+    <h1>{post.title}</h1>
+    <p>{post.body}</p>
+  {/each}
+{/if}
